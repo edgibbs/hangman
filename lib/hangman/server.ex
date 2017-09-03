0ca0e903dbd7bffe62b1eab7b9bef13cd4ba6074
Ed Gibbs <edward_gibbs@yahoo.com>
2017-09-03 16:29:27 -0700

    Implement hangman game inside a GenServer

--- lib/hangman/server.ex
+++ lib/hangman/server.ex
@@ -7,13 +7,29 @@ defmodule Hangman.Server do
     GenServer.start_link(__MODULE__, nil)
   end
 
-  def init(_) do
-    {:ok, Game.new_game() }
+  def start_link(word) do
+    GenServer.start_link(__MODULE__, word)
+  end
+
+  def init(word) do
+    start_game(word)
   end
 
   def handle_call({:make_move, guess}, _from, game) do
     { game, tally } = Game.make_move(game, guess)
     { :reply, tally, game }
+  end
+
+  def handle_call({:tally}, _from, game) do
+    { :reply, Game.tally(game), game }
+  end
+
+  defp start_game(word) when is_nil(word) do
+    {:ok, Game.new_game() }
+  end
+
+  defp start_game(word) do
+    {:ok, Game.new_game(word) }
   end
 
 end
--- test/hangman_test.exs
+++ test/hangman_test.exs
@@ -2,86 +2,113 @@ defmodule HangmanTest do
   use ExUnit.Case
 
   test "can create a new game state" do
-    game = Hangman.new_game
+    {:ok, pid } = Hangman.new_game
+    game = Hangman.tally(pid)
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert game.used == MapSet.new()
     assert length(game.letters) >= 1
-    assert Enum.all?(game.letters, fn letter -> Regex.match?(~r/[a-z]/, letter) end)
+    assert Enum.all?(game.letters, fn letter -> letter == "_" end)
   end
 
-  test "state is unchanged for won and lost game" do
-    for state <- [ :won, :lost ] do
-      game = Hangman.new_game |> Map.put(:game_state, state)
-      assert { ^game, _tally } = Hangman.make_move(game, "x")
+  @tag :capture_log
+  test "must guess with a lower case ascii letter not upper case" do
+    Process.flag :trap_exit, true
+    { :ok, game_pid } = Hangman.new_game
+    catch_exit do
+      Hangman.make_move(game_pid, "X")
     end
+    assert_received({:EXIT, ^game_pid, _})
   end
 
-  test "must guess with a lower case ascii letter" do
-    game = Hangman.new_game
-    assert_raise FunctionClauseError, fn ->
-      Hangman.make_move(game, "X")
+  @tag :capture_log
+  test "must guess with a lower case ascii letter not a number" do
+    Process.flag :trap_exit, true
+    { :ok, game_pid } = Hangman.new_game
+    catch_exit do
+      Hangman.make_move(game_pid, "1")
     end
-    assert_raise FunctionClauseError, fn ->
-      Hangman.make_move(game, "1")
-    end
+    assert_received({:EXIT, ^game_pid, _})
   end
 
   test "first occurance of a letter not already used" do
-    game = Hangman.new_game
-    { game, _tally } = Hangman.make_move(game, "x")
-    assert game.game_state != :already_used
+    { :ok, game_pid } = Hangman.new_game
+    tally = Hangman.make_move(game_pid, "x")
+    assert tally.game_state != :already_used
   end
 
   test "state is also unchanged for prior guessed letter" do
-    game = Hangman.new_game
-    { game, _tally } = Hangman.make_move(game, "x")
-    assert game.game_state != :already_used
-    { game, _tally } = Hangman.make_move(game, "x")
-    assert game.game_state == :already_used
+    { :ok, game_pid } = Hangman.new_game
+    tally = Hangman.make_move(game_pid, "x")
+    assert tally.game_state != :already_used
+    new_tally = Hangman.make_move(game_pid, "x")
+    assert new_tally.game_state == :already_used
   end
 
   test "a good guess is recognized" do
-    game = Hangman.new_game("wible")
-    { game, _tally } = Hangman.make_move(game, "w")
-    assert game.game_state == :good_guess
-    assert game.turns_left == 7
+    { :ok, game_pid } = Hangman.new_game("wible")
+
+    tally = Hangman.make_move(game_pid, "w")
+    assert tally.game_state == :good_guess
+    assert tally.turns_left == 7
   end
 
   test "guessing the whole word wins the game" do
-    game = Hangman.new_game("wible")
-    { game, _tally } = Hangman.make_move(game, "w")
-    { game, _tally } = Hangman.make_move(game, "i")
-    { game, _tally } = Hangman.make_move(game, "b")
-    { game, _tally } = Hangman.make_move(game, "l")
-    { game, _tally } = Hangman.make_move(game, "e")
-    assert game.game_state == :won
-    assert game.turns_left == 7
+    { :ok, game_pid } = Hangman.new_game("wible")
+    Hangman.make_move(game_pid, "w")
+    Hangman.make_move(game_pid, "i")
+    Hangman.make_move(game_pid, "b")
+    Hangman.make_move(game_pid, "l")
+    tally = Hangman.make_move(game_pid, "e")
+    assert tally.game_state == :won
+    assert tally.turns_left == 7
+  end
+
+  test "state is unchanged for won game" do
+    { :ok, game_pid } = Hangman.new_game("x")
+    tally = Hangman.make_move(game_pid, "x")
+    assert tally.game_state == :won
+    tally = Hangman.make_move(game_pid, "f")
+    assert tally.game_state == :won
   end
 
   test "making a bad guess" do
-    game = Hangman.new_game("burdell")
-    { game, _tally } = Hangman.make_move(game, "w")
-    assert game.turns_left == 6
-    assert game.game_state == :bad_guess
+    { :ok, game_pid } = Hangman.new_game("burdell")
+    tally = Hangman.make_move(game_pid, "w")
+    assert tally.turns_left == 6
+    assert tally.game_state == :bad_guess
   end
 
   test "losing the game" do
-    game = Hangman.new_game("z")
-    { game, _tally } = Hangman.make_move(game, "a")
-    { game, _tally } = Hangman.make_move(game, "b")
-    { game, _tally } = Hangman.make_move(game, "c")
-    { game, _tally } = Hangman.make_move(game, "d")
-    { game, _tally } = Hangman.make_move(game, "e")
-    { game, _tally } = Hangman.make_move(game, "f")
-    { game, _tally } = Hangman.make_move(game, "g")
-    assert game.game_state == :lost
+    { :ok, game_pid } = Hangman.new_game("z")
+    Hangman.make_move(game_pid, "a")
+    Hangman.make_move(game_pid, "b")
+    Hangman.make_move(game_pid, "c")
+    Hangman.make_move(game_pid, "d")
+    Hangman.make_move(game_pid, "e")
+    Hangman.make_move(game_pid, "f")
+    tally = Hangman.make_move(game_pid, "g")
+    assert tally.game_state == :lost
+  end
+
+  test "state is unchanged for lost game" do
+    { :ok, game_pid } = Hangman.new_game("z")
+    Hangman.make_move(game_pid, "a")
+    Hangman.make_move(game_pid, "b")
+    Hangman.make_move(game_pid, "c")
+    Hangman.make_move(game_pid, "d")
+    Hangman.make_move(game_pid, "e")
+    Hangman.make_move(game_pid, "f")
+    tally = Hangman.make_move(game_pid, "g")
+    assert tally.game_state == :lost
+    tally = Hangman.make_move(game_pid, "z")
+    assert tally.game_state == :lost
   end
 
   test "can get the tally of existing game" do
-    game = Hangman.new_game("wible")
-    { game, _tally } = Hangman.make_move(game, "w")
-    tally = Hangman.tally(game)
+    { :ok, game_pid } = Hangman.new_game("wible")
+    Hangman.make_move(game_pid, "w")
+    tally = Hangman.tally(game_pid)
     assert tally.game_state == :good_guess
     assert tally.turns_left == 7
     assert tally.letters == ~w[w _ _ _ _]
